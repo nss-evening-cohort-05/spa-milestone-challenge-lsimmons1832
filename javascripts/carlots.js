b147e4c1e14440763dde9c5abc1baa46664ca06a
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-25 23:59:30 -0500

    save work

--- javascripts/carlots.js
+++ javascripts/carlots.js
@@ -1,26 +1,21 @@
-var CarLot = (function() {
-    var inventory = [];
+var CarLot = (function () {
+  var inventory = [];
 
-    return {
-      loadInventory: function(callback) {
-        var inventoryLoader = new XMLHttpRequest();
-        inventoryLoader.overrideMimeType("javascripts/json");
-        inventoryLoader.open("GET", "inventory.json", true);
-        inventoryLoader.onreadystatechange = function() {
-                if (inventoryLoader.readyState === 4 && inventoryLoader.status == "200") {
+  return {
+    loadInventory: function (callback) {
+      var inventoryLoader = new XMLHttpRequest();
+			inventoryLoader.addEventListener("error", CarLot.dataFail);
+			inventoryLoader.open("GET", "javascripts/inventory.json");
+			inventoryLoader.send();
 
-                    inventoryLoader.addEventListener("load", function(text) {
-                    	callback(inventoryLoader.responseText);
-                        // var cars = JSON.parse(text);
-                        // inventory.push(callback);
-                        // callback(inventoryLoader.inventory);
-                    });
-              		inventoryLoader.send(null);
-                }
-            }
-      },
-      getInventory: function() {
-          return inventory;
-      }
-    }
+      inventoryLoader.addEventListener("load", function () {
+				var cars = JSON.parse(inventoryLoader.responseText);
+				inventory.push(cars);
+				console.log(inventory);
+     });
+    },
+    getInventory: function(){
+    	return inventory;
+  }
+}
 })();
