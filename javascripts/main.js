e3c1121170c8e0245e404ec44f5d356e16cb5c07
Latasha Simmons <lsimmons1832@gmail.com>
2017-03-26 23:32:12 -0500

    save work

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,6 +18,38 @@ function populatePage(cars) {
   // Now that the DOM is loaded, establish all the event listeners needed
   // CarLot.activateEvents();
 }
+// function highlightSelected(){
+// var section = document.getElementsByClassName("col-xs-6 col-md-4");
+// var userInput = document.getElementById("form-control");
+
+// 	for (var j = 0; j < section.length; j++) {
+// 		section[j].addEventListener("click", function(){
+// 			var sectionContainer = document.getElementsByClassName("border");
+// 			for (var i = 0; i < sectionContainer.length; i++) {
+// 				sectionContainer[i].classList.remove("border");
+// 			}
+// 			this.classList.add("border");
+// 			userInput.focus();
+// 	});
+// }
+
+// userInput.addEventListener("keypress", function(event){
+// 	var sectionContainer = document.getElementsByClassName("border");
+// 	var bioContainer = sectionContainer[0].childNodes[0].children[2];
+// 	bioContainer.innerHTML = "";
+//   var mimicMeElement = userInput.value;
+//   bioContainer.innerHTML += mimicMeElement;
+//   console.log("sectionContainer.innerHTML", sectionContainer);
+// });
+
+// window.addEventListener("keypress", function(event){
+//     if(event.keyCode==13){
+//         userInput.value = ""; 
+
+//         return false; 
+//     }
+// });
+
 // Load the inventory and send a callback function to be
 // invoked after the process is complete
 CarLot.loadInventory(populatePage);
